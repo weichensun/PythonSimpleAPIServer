352fa6d9e547e82344f9134e19b376855d0f0952
Wei-Chen Sun <weichensun@Wei-Chens-MacBook.local>
2016-11-26 15:35:38 +0800

    Remove route

--- route/_route_rules.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from _core.router import load_worker
-
-def get_worker( path ):
-    if path == '/':
-        return load_worker( 'route.index' )
-    return None
