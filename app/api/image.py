a417e5bc5b7b185da1b0389515f26517ba87ab78
weichensun <sunweichen2000@hotmail.com>
2018-09-11 17:24:41 +0800

    Remove text

--- app/api/image.py
+++ app/api/image.py
@@ -6,8 +6,5 @@ from _core.base_worker import BaseWorker
 class Image(BaseWorker):
 
     def do_GET(self):
-        # We use ThreadedHTTPServer
-        # A long task will not block other service request
         path = 'image/lenna.png'
-
         return self.replyFile(path)
