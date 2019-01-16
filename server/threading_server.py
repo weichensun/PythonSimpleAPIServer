b0cad4d1606d74dd232dafd36f4faaf10577bc9a
weichensun <sunweichen2000@hotmail.com>
2019-01-16 18:51:09 +0800

    Remove commented code

--- server/threading_server.py
+++ server/threading_server.py
@@ -3,10 +3,8 @@
 
 import sys
 if sys.version_info[0] < 3:
-#    from BaseHTTPServer import HTTPServer
     import SocketServer
 else:
-#    from http.server import HTTPServer
     import socketserver as SocketServer
 
 from .http_server import HTTPServer
